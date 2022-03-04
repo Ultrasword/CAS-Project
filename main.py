a32e4be633125036f15ce14c6dc0811a5836a77e
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-03-04 17:47:50 -0500

    wadawd

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ import traceback
 
 from pygame._sdl2.video import Window as sdlWindow
 
+from bin.engine import window, clock, event, state, ui, camera
+
 from bin import game
-from bin.game import components, tasks
-from bin.engine import *
-from bin.engine import state, particle, event, taskqueue, ui, multiprocesshandling
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
-ChunkObject = handler.Chunk
 
 
 def main():
     # create all necessary objects
-    window_scale_size = (1920, 1080)
+    window_scale_size = (1280, 720)
     window_blit_location = (0, 0)
     RENDER_DISTANCE = 2
     BACKGROUND_COLOR = (255, 255, 255)
@@ -27,36 +25,19 @@ def main():
     Window.sdlwindow = sdlWindow.from_display_module()
     Window.set_min_size(1280, 720)
     Clock = clock.Clock(60)
-    MemoryStorage = multiprocesshandling.MultiThreadedTaskHandler(max_threads=2)
     frame_buffer = pygame.Surface(window_scale_size, pygame.SRCALPHA, 32).convert()
     running = True
 
-    try:
-        # init engine
-        game.init()
-        state.init(camera=camera.Camera(window_size=window_scale_size), mp=MemoryStorage)
-        state.USER = ui.User()
-        MemoryStorage.start(state.WORLD)
 
-        state.push_state(
-            game.gamestates.InGame(MemoryStorage, camera_pos=[window_scale_size[0] // 2, window_scale_size[1] // 2],
-                                   seed=1))
-        # add some preloaded chunks
-        for x in range(3):
-            for y in range(3):
-                state.WORLD.add_chunk(handler.Chunk(x, y, terrain=pygame.image.load(f"assets/test/{x}.{y}.png").convert()), make=False)
+    # create a camera
+    CAMERA = camera.Camera(window_scale_size)
 
-        state.WORLD.calculate_relavent_chunks(state.CAMERA.chunkpos, RENDER_DISTANCE)
-        state.WORLD.get_chunk("0.0").add_block(["assets/kirb.jpeg", 400, 400, 100, 100, filehandler.size_to_num((100,100))])
-        state.CURRENT_STATE.add_entity(game.warrior.Warrior((300, 300), frame=0))
-        # add systems
-        state.CURRENT_STATE.add_system(0, components.UserDragVisualiser())
-        state.USER.mouse.update_ratio(1280, 720, window_scale_size[0], window_scale_size[1])
-    except Exception as e:
-        print(traceback.print_exc())
-        # exit the program
-        running = False
-        MemoryStorage.end()
+    # make a state
+    state.init(CAMERA, None)
+    state.USER = ui.User()
+    state.push_state(state.GameState(None))
+    # state.CURRENT_STATE.add_entity()
+
 
     # run processes
     Clock.start()
@@ -66,7 +47,6 @@ def main():
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 running = False
-                MemoryStorage.end()
                 break
             # window resize
             elif e.type == pygame.WINDOWRESIZED:
@@ -97,17 +77,17 @@ def main():
             else:
                 # do later
                 pass
-
-        # get camera moving
-        state.CURRENT_STATE.update_cam(Clock.delta_time, state.CAMERA)
-
+        
         # render stuff and update everything
         frame_buffer.fill(BACKGROUND_COLOR)
-
         # render with camera
-        state.CURRENT_STATE.update_systems(Clock.delta_time)
-        state.CAMERA.render_and_update_with_camera(frame_buffer, Clock.delta_time)
-        state.CURRENT_STATE.render_systems(frame_buffer)
+        if state.CURRENT_STATE:
+                # get camera moving
+            state.CURRENT_STATE.update_cam(Clock.delta_time, state.CAMERA)
+
+            state.CURRENT_STATE.update_systems(Clock.delta_time)
+            state.CAMERA.render_and_update_with_camera(frame_buffer, Clock.delta_time)
+            state.CURRENT_STATE.render_systems(frame_buffer)
 
         # render the frame_buffer onto the screen
         Window.window.blit(pygame.transform.scale(frame_buffer, Window.area), window_blit_location)
@@ -115,9 +95,6 @@ def main():
         pygame.display.flip()
         Clock.update()
         Clock.wait()
-    MemoryStorage.close()
-    state.WORLD.clear_cache(handler.CACHE_PATH)
-
 
 if __name__ == "__main__":
     main()
